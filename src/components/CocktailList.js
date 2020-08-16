4c0b31660560605e696ccf7a1e10d1a9147378b8
Gayhun <leeg23@student.douglascollege.ca>
2020-08-16 15:37:01 -0700

    CocktailList shows list and responsive css file added

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from "react";
+import { Card } from "antd";
+import '../styles/CocktailList.css';
+
+const { Meta } = Card;
 
 const CocktailList = (props) => {
-  
-  const cocktails = props.cocktails.map(({strDrink, strDrinkThumb, idDrink}) => <div key={idDrink}>
-    <img width = "50px" alt={strDrink} src={strDrinkThumb}/>
-    <h2>{strDrink}</h2>
-  </div>);
+  const cocktails = props.cocktails.map(
+    ({ strDrink, strDrinkThumb, idDrink }) => (
+      <Card
+        key={idDrink}
+        hoverable
+        style={{ width: '20%', margin: '2%', float:'left'}}
+        cover={
+          <img
+            alt={strDrink}
+            src={strDrinkThumb}
+          />
+        }
+      >
+        <Meta style={{ fontSize: '1.5vw' }} title={strDrink} description={idDrink} />
+      </Card>
+    )
+  );
 
-  return <div>{cocktails}</div>
+  return <div>{cocktails}</div>;
 };
 // https://ant.design/components/card/#
 export default CocktailList;
+
